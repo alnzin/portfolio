2a21765033b213df86d521c4db8f205314ba3130
alnzin <alann.life@gmail.com>
2016-03-01 20:25:13 -0300

    versão inicial

---
+++ js/codigo.js
@@ -0,0 +1,14 @@
+	function t1(){
+    document.getElementById('tab-2').style.display = 'none';
+    document.getElementById('tab-1').style.display = 'block';
+}
+
+	function t2(){
+    document.getElementById('tab-1').style.display = 'none';
+    document.getElementById('tab-2').style.display = 'block';
+}
+
+	function ft(){
+	document.getElementById('ft1').style.transform= "rotate(500grad)";	
+	document.getElementById('ft1').style.transition= "3s ease";
+}
